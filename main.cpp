c45beee1e0f54bcab6097f13576d803c6678056d
Cason <casonmartz@icloud.com>
2021-11-05 18:57:53 -0700

    Removed bad if else ladders, reduced unneeded junk

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <stdio.h>
-#include <vector>
 #include <random>
 
 using namespace std;
@@ -8,60 +6,13 @@ using namespace std;
 class alphabet{
 public:
 	char letters[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	char a = 'a';
-	char b = 'b';
-	char c = 'c';
-	char d = 'd';
-	char e = 'e';
-	char f = 'f';
-	char g = 'g';
-	char h = 'h';
-	char i = 'i';
-	char j = 'j';
-	char k = 'k';
-	char l = 'l';
-	char m = 'm';
-	char n = 'n';
-	char o = 'o';
-	char p = 'p';
-	char q = 'q';
-	char r = 'r';
-	char s = 's';
-	char t = 't';
-	char u = 'u';
-	char v = 'v';
-	char w = 'w';
-	char x = 'x';
-	char y = 'y';
-	char z = 'z';
+	char legend[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	
 	void allVariables(){
-		randomInator(a);
-		randomInator(b);
-		randomInator(c);
-		randomInator(d);
-		randomInator(e);
-		randomInator(f);
-		randomInator(g);
-		randomInator(h);
-		randomInator(i);
-		randomInator(j);
-		randomInator(k);
-		randomInator(l);
-		randomInator(m);
-		randomInator(n);
-		randomInator(o);
-		randomInator(p);
-		randomInator(q);
-		randomInator(r);
-		randomInator(s);
-		randomInator(t);
-		randomInator(u);
-		randomInator(v);
-		randomInator(w);
-		randomInator(x);
-		randomInator(y);
-		randomInator(z);
+		for (int i = 0; i <= 25; ++i){
+			randomInator(legend[i]);
+		}
+		
 		return;
 	}
 	
@@ -81,146 +32,31 @@ public:
 	}
 	
 	void encode(string message, string& encodedMessage){
-		for (int ie = 0; ie <= message.length(); ++ie) {
-			if (message[ie] == 'a') {
-				encodedMessage += a;
-			} else if (message[ie] == 'b') {
-				encodedMessage += b;
-			} else if (message[ie] == 'c') {
-				encodedMessage += c;
-			} else if (message[ie] == 'd') {
-				encodedMessage += d;
-			} else if (message[ie] == 'e') {
-				encodedMessage += e;
-			} else if (message[ie] == 'f') {
-				encodedMessage += f;
-			} else if (message[ie] == 'g') {
-				encodedMessage += g;
-			} else if (message[ie] == 'h') {
-				encodedMessage += h;
-			} else if (message[ie] == 'i') {
-				encodedMessage += i;
-			} else if (message[ie] == 'j') {
-				encodedMessage += j;
-			} else if (message[ie] == 'k') {
-				encodedMessage += k;
-			} else if (message[ie] == 'l') {
-				encodedMessage += l;
-			} else if (message[ie] == 'm') {
-				encodedMessage += m;
-			} else if (message[ie] == 'n') {
-				encodedMessage += n;
-			} else if (message[ie] == 'o') {
-				encodedMessage += o;
-			} else if (message[ie] == 'p') {
-				encodedMessage += p;
-			} else if (message[ie] == 'q') {
-				encodedMessage += q;
-			} else if (message[ie] == 'r') {
-				encodedMessage += r;
-			} else if (message[ie] == 's') {
-				encodedMessage += s;
-			} else if (message[ie] == 't') {
-				encodedMessage += t;
-			} else if (message[ie] == 'u') {
-				encodedMessage += u;
-			} else if (message[ie] == 'v') {
-				encodedMessage += v;
-			} else if (message[ie] == 'w') {
-				encodedMessage += w;
-			} else if (message[ie] == 'x') {
-				encodedMessage += x;
-			} else if (message[ie] == 'y') {
-				encodedMessage += y;
-			} else if (message[ie] == 'z') {
-				encodedMessage += z;
+		for (int i = 0; i < message.length(); ++i) {
+			if (isspace(message[i])){
+				encodedMessage += ' ';
+			}
+			else {
+				encodedMessage += legend[int(message[i]) - 96];
 			}
 		}
 		return;
 	}
 	
 void decode(string encodedMessage, string& message){
-	for (int ie = 0; ie <= encodedMessage.length(); ++ie) {
-		if (encodedMessage[ie] == 'a') {
-			message += a;
+	for (int i = 0; i <= encodedMessage.length(); ++i) {
+		if (isspace(encodedMessage[i])){
+			message += ' ';
 		}
-		else if (encodedMessage[ie] == b) {
-			message += 'b';
+		else {
+			for (int e = 0; e <= 25; ++e){
+				if (encodedMessage[i] == legend[e]){
+					message += letters[e];
+					break;
+				}
+			}
+			
 		}
-		else if (encodedMessage[ie] == c) {
-			message += 'c';
-		}
-		else if (encodedMessage[ie] == d) {
-			message += 'd';
-		}
-		else if (encodedMessage[ie] == e) {
-			message += 'e';
-		}
-		else if (encodedMessage[ie] == f) {
-			message += 'f';
-		}
-		else if (encodedMessage[ie] == g) {
-			message += 'g';
-		}
-		else if (encodedMessage[ie] == h) {
-			message += 'h';
-		}
-		else if (encodedMessage[ie] == i) {
-			message += 'i';
-		}
-		else if (encodedMessage[ie] == j) {
-			message += 'j';
-		}
-		else if (encodedMessage[ie] == k) {
-			message += 'k';
-		}
-		else if (encodedMessage[ie] == l) {
-			message += 'l';
-		}
-		else if (encodedMessage[ie] == m) {
-			message += 'm';
-		}
-		else if (encodedMessage[ie] == n) {
-			message += 'n';
-		}
-		else if (encodedMessage[ie] == o) {
-			message += 'o';
-		}
-		else if (encodedMessage[ie] == p) {
-			message += 'p';
-		}
-		else if (encodedMessage[ie] == q) {
-			message += 'q';
-		}
-		else if (encodedMessage[ie] == r) {
-			message += 'r';
-		}
-		else if (encodedMessage[ie] == s) {
-			message += 's';
-		}
-		else if (encodedMessage[ie] == t) {
-			message += 't';
-		}
-		else if (encodedMessage[ie] == u) {
-			message += 'u';
-		}
-		else if (encodedMessage[ie] == v) {
-			message += 'v';
-		}
-		else if (encodedMessage[ie] == w) {
-			message += 'w';
-		}
-		else if (encodedMessage[ie] == x) {
-			message += 'x';
-		}
-		else if (encodedMessage[ie] == y) {
-			message += 'y';
-		}
-		else if (encodedMessage[ie] == z) {
-			message += 'z';
-		}
-		
-		
 	}
 	return;
 }
@@ -261,12 +97,15 @@ void decode(string encodedMessage, string& message){
 
 
 int main() {
-	alphabet basic, encode;
+	alphabet encode;
 	string message, encodedMessage, decodedMessage;
 	char encodeDecode = 'i';
 	encode.allVariables();
 	
 	do {
+		message = "";
+		encodedMessage = "";
+		decodedMessage = "";
 		cout << "Would you like to decode or encode? [e/d] ([x] to exit): ";
 		cin >> encodeDecode;
 		while (encodeDecode != 'e' && encodeDecode != 'd' && encodeDecode != 'x'){
@@ -276,9 +115,13 @@ int main() {
 		}
 		
 		if (encodeDecode == 'e') {
+			cout << endl;
 			encode.inputEncode(message, encodedMessage);
+			cout << endl;
 		} else if (encodeDecode == 'd') {
-			encode.decodeFunc(encodedMessage, decodedMessage);
+			cout << endl;
+			encode.inputDecodeFunc(decodedMessage);
+			cout << endl;
 		}
 	} while (encodeDecode != 'x');
 	
